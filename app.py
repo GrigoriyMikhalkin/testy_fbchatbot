b924bfce87b0c638f6ea816e727ba8fd3a0dc54d
GrigoriyMikhalkin <mikgrigoriy@yandex.ru>
2017-07-05 08:18:44 +0300

    qf

--- app.py
+++ app.py
@@ -83,7 +83,10 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
-def log(message):  # simple wrapper for logging to stdout on heroku
+def log(message):
+    """
+    Simple wrapper for logging to stdout on heroku
+    """
     print(str(message))
     sys.stdout.flush()
 
